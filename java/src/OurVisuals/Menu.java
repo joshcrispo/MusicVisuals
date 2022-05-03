569bd95be27f8dbfcf338a163a09229c07c375b4
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2022-05-03 17:53:45 +0100

    josh stuff trying to figure out stuff

--- java/src/OurVisuals/Menu.java
+++ java/src/OurVisuals/Menu.java
@@ -8,22 +8,21 @@ public class Menu extends Visual {
     // array to store song names
     String[] display_names = { "Homies In Paris", "Hero Planet", "Masquerade" };
     // array to store file names
-    String[] songs = { "HomiesInParis2.mp3", "heroplanet.mp3", "Masquerade.mp3" };
+    String[] songs = { "java/data/HomiesInParis2.mp3", "java/data/heroplanet.mp3", "java/data/Masquerade.mp3" };
     // index of the song selected
     int song_index = 0;
     // array to store number of playes
     String[] players = { "1 player", "2 players" };
-    int width = 800;
-    int middleW = width / 2;
     int border = 120;
     int gap = 50;
     int arrowH = 30;
     float lerpedAverage = 0;
     Lines lines;
+    Stage stage;
 
     public void settings() {
 
-        size(800, 800);
+        size(1024, 1000);
         // fullScreen(P3D, SPAN);
     }
 
@@ -49,7 +48,7 @@ public class Menu extends Visual {
 
     public void mousePressed() {
 
-        if (mouseX > middleW + border && mouseX < middleW + border + arrowH && mouseY > border * 2 + 35
+        if (mouseX > width/2 + border && mouseX < width/2 + border + arrowH && mouseY > border * 2 + 35
                 && mouseY < border * 2 + 35 + arrowH / 2) {
 
             song_index += 1;
@@ -57,29 +56,35 @@ public class Menu extends Visual {
             println(songs.length);
 
             if (song_index > songs.length - 1) {
-                song_index -= 1;
+                song_index = 0;
             }
 
             play_music();
 
         }
-
-        if (mouseX > border * 2 + 10 && mouseX < border * 2 + 10 + arrowH && mouseY > border * 2 + arrowH
-                && mouseY < border * 2 + arrowH + arrowH / 2) {
+                
+        if (mouseX > border * 2.9 + 10 && mouseX < border * 2.9 + 10 + arrowH && mouseY > border * 2 + arrowH
+                && mouseY < border * 2 + arrowH + arrowH) {
 
             song_index -= 1;
             if (song_index < 0) {
-                song_index += 1;
+                song_index = 2;
             }
 
             play_music();
+
+        }
+        if (mouseX < width/2 + arrowH+2.5f && mouseX > width/2 - arrowH-2.5f && mouseY < border * 5
+                && mouseY > border * 2 - 20) {
+
+           exit();
 
         }
 
     }
 
     // draw arrows
-    void drawArrow(int x, int y, int size, float angle) {
+    void drawArrow(float x, int y, int size, float angle) {
 
         pushMatrix();
         translate(x, y);
@@ -96,34 +101,37 @@ public class Menu extends Visual {
         textAlign(CENTER);
         textSize(40);
         fill(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        text("Game of Notes", middleW, border);
+        text("Game of Notes", width/2, border);
         fill(255);
         textSize(30);
-        text("Choose Song", middleW, border * 2);
-        text("Number of Players", middleW, border * 3);
-        text("Start", middleW, border * 5);
+        text("Choose Song", width/2, border * 2);
+        text("Number of Players", width/2, border * 3);
+        text("Start", width/2, border * 5);
         textSize(25);
         // display song name here
-        text(display_names[song_index], middleW, border * 2 + gap);
+        text(display_names[song_index], width/2, border * 2 + gap);
         // display number of player here
-        text(players[0], middleW, border * 3 + gap);
+        text(players[0], width/2, border * 3 + gap);
 
     }
 
     public void draw() {
 
         background(0);
+        textSize(20);
+        text(mouseX,width/2,500);
+        text(mouseY,width/2,550);
         calculateAverageAmplitude();
         calculateFrequencyBands();
         drawMainScreen();
         // change text colour with amplitude
         stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
         // change this after
-        drawArrow(middleW + border, border * 2 + gap - 8, arrowH, 0);
-        drawArrow(border * 2 + gap - 8, border * 2 + gap - 8, arrowH, 179);
-        drawArrow(middleW + border, border * 3 + gap - 8, arrowH, 0);
-        drawArrow(border * 2 + gap - 8, border * 3 + gap - 8, arrowH, 179);
+        drawArrow(width/2 + border, border * 2 + gap - 8, arrowH, 0);
+        drawArrow(border * 2.9f + gap - 8, border * 2 + gap - 8, arrowH, 179);
+        drawArrow(width/2 + border, border * 3 + gap - 8, arrowH, 0);
+        drawArrow(border * 2.9f + gap - 8, border * 3 + gap - 8, arrowH, 179);
         lines.draw_lines();
-
+        stage.draw();
     }
 }
